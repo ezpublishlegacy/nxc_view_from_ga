32ea21e098ae1509a57f3d837a9bc5c9c2e7414d
Alex Pilyavskiy <spi@nxc.no>
2013-07-04 12:36:53 +0300

    change extension name in settings

--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -1,7 +1,7 @@
 <?php /* #?ini charset="iso-8859-1"?
 
 [CronjobSettings]
-ExtensionDirectories[]=nxc_views_from_ga
+ExtensionDirectories[]=nxc_view_from_ga
 
 [CronjobPart-ga_views]
 Scripts[]=update_view_count.php
